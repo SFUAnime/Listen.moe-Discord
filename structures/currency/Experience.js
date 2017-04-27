13a2adade513168844395755bb3f4bde7f44d031
vzwGrey <thegreylp@googlemail.com>
2017-04-27 14:53:14 +0200

    Fix db updating

--- structures/currency/Experience.js
+++ structures/currency/Experience.js
@@ -60,7 +60,7 @@ class Experience {
 					experience: experiences[id]
 				});
 			} else {
-				await user.save({ experience: experiences[id] });
+				await user.update({ experience: experiences[id] });
 			}
 		}
 		/* eslint-enable no-await-in-loop */
