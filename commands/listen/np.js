cdbede8fc2f00ccfd881394c8be08feccc72dc6f
kosterlitz <vortexunbinding@gmail.com>
2017-05-04 20:39:44 -0700

    np escapeMarkdown

--- commands/listen/np.js
+++ commands/listen/np.js
@@ -30,7 +30,7 @@ module.exports = class NowPlayingCommand extends Command {
 		const anime = radioInfo.anime_name ? `Anime: ${radioInfo.anime_name}` : '';
 		const requestedBy = radioInfo.requested_by
 			? /\s/g.test(radioInfo.requested_by)
-			? `🎉 **${Discord.escapeMarkdown(radioInfo.requested_by)}** 🎉`
+				? `🎉 **${Discord.escapeMarkdown(radioInfo.requested_by)}** 🎉`
 			: `Requested by: [${Discord.escapeMarkdown(radioInfo.requested_by)}](https://forum.listen.moe/u/${radioInfo.requested_by})`
 			: ''; //the markdown for requested by needs to be escaped carefully to avoid escaping out the special event ** markdown
 		const song = `${Discord.escapeMarkdown(nowplaying)}\n\n${Discord.escapeMarkdown(anime)}\n${requestedBy}`;
